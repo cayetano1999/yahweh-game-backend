d25e573269cd39be6b3b8aac11d4729cb224d556
jcayetano <jcayetano@apap.com.do>
2024-03-15 11:22:52 -0400

    Crud de reglas con nuevas propiedades

--- src/modules/rule/interfaces/rule.interface.ts
+++ src/modules/rule/interfaces/rule.interface.ts
@@ -1,6 +1,15 @@
-import { Document } from 'mongoose';
+import { Document, Schema } from 'mongoose';
+import { RuleFact } from './rule-fact.interface';
 
 export interface Rule extends Document {
-  readonly name: string;
-  readonly value: string;
+
+  readonly name: string,
+  readonly description: string;
+  readonly active: boolean,
+  readonly value: { type: Schema.Types.Mixed },
+  readonly creationDate: Date,
+  readonly createBy: string,
+  readonly updateBy: string,
+  readonly ruleFacts: { type: Schema.Types.Mixed }
 }
+
--- src/modules/rule/schemas/rule.schema.ts
+++ src/modules/rule/schemas/rule.schema.ts
@@ -1,6 +1,14 @@
 import * as mongoose from 'mongoose';
+import { Schema } from 'mongoose';
 
 export const RuleSchema = new mongoose.Schema({
   name: String,
-  value: mongoose.Schema.Types.Mixed,
+  active: Boolean,
+  value: { type: Schema.Types.Mixed },
+  creationDate: Date,
+  createBy: String,
+  updateBy: String,
+  description: String,
+  ruleFacts: { type: Schema.Types.Mixed }
+
 });
