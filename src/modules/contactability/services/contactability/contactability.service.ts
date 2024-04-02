356ec9ccf528d7489939314549f0446b2144f63c
jcayetano <jcayetano@apap.com.do>
2024-04-02 12:05:41 -0400

    contactability url

--- src/modules/contactability/services/contactability/contactability.service.ts
+++ src/modules/contactability/services/contactability/contactability.service.ts
@@ -5,14 +5,14 @@ import { HttpService } from '@nestjs/axios';
 
 const routes = {
   root: process.env.CONTATABILITY_URL_API,
-  getAllTemplates: (chanelType) => `${routes.root}/template?channelType=${chanelType}`
+  getAllTemplates: () => `https://contactability-service-programa-digitalizacion-dev.apps.x966bdcl.eastus2.aroapp.io/api/template?channelType=10`
 }
 @Injectable()
 export class ContactabilityService {
   constructor(private readonly httpService: HttpService) {}
 
   getTemplates(): Promise<any> {
-    const url = routes.getAllTemplates(10);
+    const url = routes.getAllTemplates();
     return new Promise((resolve, rejects) => {
       this.httpService.get(url).subscribe({
         next: (value) => {
