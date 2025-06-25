6f7ad2699ad6632ded07936c8e7ad4a9007a8cac
jcayetano <jcayetano@apap.com.do>
2025-06-24 20:29:11 -0400

    change data source

--- data-source.ts
+++ data-source.ts
@@ -4,7 +4,7 @@ export const AppDataSource = new DataSource({
   type: 'postgres',
   host: "postgresql-198815-0.cloudclusters.net", //process.env.DATABASE_HOST || 'localhost',
   port: 19991, //parseInt(process.env.DATABASE_PORT, 10) || 5432,
-  username: "yahweh", //process.env.DATABASE_USER || 'cayetano',
+  username: "yahweh", //process.env.DATABASE_USER ,
   password: "ada35111ff", //process.env.DATABASE_PASSWORD || 'ada35111ff',
   database: "yahweh", //process.env.DATABASE_NAME || 'yahweh-game',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
